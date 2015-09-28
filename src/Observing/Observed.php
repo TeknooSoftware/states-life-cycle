2935b06cf3f418430effe7062ca1815b93327c4a
Richard Deloge <r.deloge@uni-alteri.com>
2015-09-28 18:37:45 +0200

    update licenses

--- src/Observing/Observed.php
+++ src/Observing/Observed.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * States.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@uni-alteri.com so we can send you a copy immediately.
+ *
+ * @copyright   Copyright (c) 2009-2015 Uni Alteri (http://uni-alteri.com)
+ *
+ * @link        http://teknoo.it/states Project website
+ *
+ * @license     http://teknoo.it/license/mit         MIT License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ */
+
 namespace UniAlteri\States\LifeCycle\Observing;
 
 use UniAlteri\States\LifeCycle\Event\Event;
@@ -11,7 +30,13 @@ use UniAlteri\States\LifeCycle\Trace\TraceInterface;
 
 /**
  * Class Observed
- * @package UniAlteri\States\LifeCycle\Observing
+ *
+ * @copyright   Copyright (c) 2009-2015 Uni Alteri (http://uni-alteri.com)
+ *
+ * @link        http://teknoo.it/states Project website
+ *
+ * @license     http://teknoo.it/license/mit         MIT License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
  */
 class Observed implements ObservedInterface
 {
--- src/Observing/ObserverInterface.php
+++ src/Observing/ObserverInterface.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * States.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@uni-alteri.com so we can send you a copy immediately.
+ *
+ * @copyright   Copyright (c) 2009-2015 Uni Alteri (http://uni-alteri.com)
+ *
+ * @link        http://teknoo.it/states Project website
+ *
+ * @license     http://teknoo.it/license/mit         MIT License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ */
+
 namespace UniAlteri\States\LifeCycle\Observing;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -8,7 +27,13 @@ use UniAlteri\States\LifeCycle\Tokenization\TokenizerInterface;
 
 /**
  * Interface ObserverInterface
- * @package UniAlteri\States\LifeCycle\Observing
+ *
+ * @copyright   Copyright (c) 2009-2015 Uni Alteri (http://uni-alteri.com)
+ *
+ * @link        http://teknoo.it/states Project website
+ *
+ * @license     http://teknoo.it/license/mit         MIT License
+ * @author      Richard Déloge <r.deloge@uni-alteri.com>
  */
 interface ObserverInterface
 {
