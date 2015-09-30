b23c123518fe4e8bfcef8eb442145d0a1bd18241
Richard Deloge <r.deloge@uni-alteri.com>
2015-09-30 19:35:50 +0200

    write unit tests

--- tests/Tokenization/TokenizerTest.php
+++ tests/Tokenization/TokenizerTest.php
@@ -21,7 +21,9 @@
 
 namespace UniAlteri\Tests\States\LifeCycle\Tokenization;
 
+use UniAlteri\States\LifeCycle\Event\EventInterface;
 use UniAlteri\States\LifeCycle\Tokenization\Tokenizer;
+use UniAlteri\Tests\States\LifeCycle\StatedClass\Support\Acme\Acme;
 
 /**
  * Class TokenizerTest.
@@ -43,5 +45,70 @@ class TokenizerTest extends AbstractTokenizerTest
     public function build()
     {
         return new Tokenizer();
+    }
+
+    /**
+     * @expectedException \TypeError
+     */
+    public function testGetStatedClassNameTokenBadArg()
+    {
+        $this->build()->getStatedClassNameToken(new \stdClass());
+    }
+
+    public function testGetStatedClassNameToken()
+    {
+        $this->assertEquals(
+            'unialteri_tests_states_lifecycle_statedclass_support_acme',
+            $this->build()->getStatedClassNameToken('UniAlteri\Tests\States\LifeCycle\StatedClass\Support\Acme')
+        );
+    }
+
+    public function testGetStatedClassNameTokenFull()
+    {
+        $this->assertEquals(
+            'unialteri_tests_states_lifecycle_statedclass_support_acme',
+            $this->build()->getStatedClassNameToken('UniAlteri\Tests\States\LifeCycle\StatedClass\Support\Acme\Acme', true)
+        );
+    }
+
+    /**
+     * @expectedException \TypeError
+     */
+    public function testGetStatedClassInstanceTokenBadArg()
+    {
+        $this->build()->getStatedClassInstanceToken(new \stdClass());
+    }
+
+    public function testGetStatedClassInstanceToken()
+    {
+        $this->assertEquals(
+            'unialteri_tests_states_lifecycle_statedclass_support_acme',
+            $this->build()->getStatedClassInstanceToken(new Acme())
+        );
+    }
+
+    public function testGetTokenValue()
+    {
+        /**
+         * @var EventInterface|\PHPUnit_Framework_MockObject_MockObject
+         */
+        $instance = $this->getMock('UniAlteri\States\LifeCycle\Event\EventInterface');
+        $instance->expects($this->any())->method('getObject')->willReturn(new Acme());
+        $instance->expects($this->any())->method('getEnabledStates')->willReturn(['state1', 'state2', 'state3']);
+        $instance->expects($this->any())->method('getIncomingStates')->willReturn(['state1', 'state2']);
+        $instance->expects($this->any())->method('getOutgoingStates')->willReturn(['state4']);
+
+        $this->assertEquals(
+            [
+                'unialteri_tests_states_lifecycle_statedclass_support_acme',
+                'unialteri_tests_states_lifecycle_statedclass_support_acme:state1',
+                'unialteri_tests_states_lifecycle_statedclass_support_acme:state2',
+                'unialteri_tests_states_lifecycle_statedclass_support_acme:state3',
+                'unialteri_tests_states_lifecycle_statedclass_support_acme:+state1',
+                'unialteri_tests_states_lifecycle_statedclass_support_acme:+state2',
+                'unialteri_tests_states_lifecycle_statedclass_support_acme:-state4'
+            ],
+            $this->build()->getToken($instance)
+        );
     }
 }
