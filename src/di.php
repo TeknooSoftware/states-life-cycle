a73b7d6da6e491693670e71b4bdeb0f56701aa05
Richard Deloge <richarddeloge@gmail.com>
2017-07-04 00:55:49 +0200

    prepare di file to use with php-di

--- src/di.php
+++ src/di.php
@@ -22,8 +22,10 @@
 
 namespace Teknoo\States\LifeCycle;
 
-use Psr\Container\ContainerInterface;
+use function DI\get;
+use Gaufrette\Filesystem;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\Yaml\Parser;
 use Teknoo\States\LifeCycle\Event\EventDispatcherBridgeInterface;
 use Teknoo\States\LifeCycle\Event\Symfony\Event;
 use Teknoo\States\LifeCycle\Event\Symfony\EventDispatcherBridge;
@@ -34,36 +36,81 @@ use Teknoo\States\LifeCycle\Observing\Observer;
 use Teknoo\States\LifeCycle\Observing\ObserverInterface;
 use Teknoo\States\LifeCycle\Scenario\Manager;
 use Teknoo\States\LifeCycle\Scenario\ManagerInterface;
+use Teknoo\States\LifeCycle\Scenario\Scenario;
+use Teknoo\States\LifeCycle\Scenario\ScenarioBuilder;
+use Teknoo\States\LifeCycle\Scenario\ScenarioBuilderInterface;
+use Teknoo\States\LifeCycle\Scenario\ScenarioInterface;
+use Teknoo\States\LifeCycle\Scenario\ScenarioYamlBuilder;
 use Teknoo\States\LifeCycle\Tokenization\Tokenizer;
 use Teknoo\States\LifeCycle\Tokenization\TokenizerInterface;
 use Teknoo\States\LifeCycle\Trace\Trace;
 
 return [
-    TokenizerInterface::class => function (): TokenizerInterface {
+    //Tokenizer
+    Tokenizer::class => function (): TokenizerInterface {
         return new Tokenizer();
     },
+    TokenizerInterface::class => get(Tokenizer::class),
 
-    EventDispatcherBridgeInterface::class => function (ContainerInterface $container): EventDispatcherBridgeInterface {
-        return new EventDispatcherBridge($container->get(EventDispatcher::class));
+    //Event dispatcher
+    EventDispatcherBridge::class => function (EventDispatcher $eventDispatcher): EventDispatcherBridgeInterface {
+        return new EventDispatcherBridge($eventDispatcher);
     },
+    EventDispatcherBridgeInterface::class => get(EventDispatcherBridge::class),
 
-    ManagerInterface::class => function (ContainerInterface $container): ManagerInterface {
-        return new Manager($container->get(EventDispatcherBridgeInterface::class));
+    //Manager
+    Manager::class => function (EventDispatcherBridgeInterface $bridge): ManagerInterface {
+        return new Manager($bridge);
     },
+    ManagerInterface::class => get(Manager::class),
 
-    ObservedFactoryInterface::class => function (): ObservedFactoryInterface {
+    //Observed factory
+    ObservedFactory::class => function (): ObservedFactoryInterface {
         return new ObservedFactory(
             Observed::class,
             Event::class,
             Trace::class
         );
     },
+    ObservedFactoryInterface::class => get(ObservedFactory::class),
 
-    ObserverInterface::class => function (ContainerInterface $container): ObserverInterface {
-        $observer = new Observer($container->get(ObservedFactoryInterface::class));
-        $observer->addEventDispatcher($container->get(EventDispatcherBridgeInterface::class));
-        $observer->setTokenizer($container->get(TokenizerInterface::class));
+    //observer
+    Observer::class => function (
+        ObservedFactoryInterface $factory,
+        EventDispatcherBridgeInterface $bridge,
+        TokenizerInterface $tokenizer
+    ): ObserverInterface {
+
+        $observer = new Observer($factory);
+        $observer->addEventDispatcher($bridge);
+        $observer->setTokenizer($tokenizer);
     
         return $observer;
     },
+    ObserverInterface::class => get(Observer::class),
+
+    //Scenario builder generator
+    ScenarioBuilder::class => function(Tokenizer $tokenizer) {
+        return function () use ($tokenizer) {
+            return new ScenarioBuilder($tokenizer);
+        };
+    },
+    ScenarioBuilderInterface::class => get(ScenarioBuilder::class),
+
+    //Scenario yaml builder generator
+    ScenarioYamlBuilder::class => function (Tokenizer $tokenizer, Parser $parser, Filesystem $filesystem) {
+        return function () use ($tokenizer, $parser, $filesystem) {
+            return (new ScenarioYamlBuilder($tokenizer))
+                ->setYamlParser($parser)
+                ->setFilesystem($filesystem);
+        };
+    },
+
+    //Scenario builder
+    Scenario::class => function () {
+        return function () {
+            return new Scenario();
+        };
+    },
+    ScenarioInterface::class => get(Scenario::class),
 ];
